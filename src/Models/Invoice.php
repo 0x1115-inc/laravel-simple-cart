a00e2d6656af0b1bffed8356f7c31c18829f7b1a
Duog Lee <duoglee@0x1115.com>
2025-08-07 17:11:42 +0700

    Add model classes for CryptoPayment, Customer, Invoice, Order, OrderItem, Payment, and Product

---
+++ src/Models/Invoice.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Copyright 2025 0x1115 Inc <info@0x1115.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace MCXV\SimpleCart\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invoice extends Model
+{
+    protected $table = 'sc_invoices';
+    
+    protected $fillable = [
+        'invoice_number',
+        'issue_date',
+        'due_date',
+        'notes',
+        'total_amount',
+        'status', 
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(Payment::class);
+    }
+}
---
+++ src/Models/Order.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright 2025 0x1115 Inc <info@0x1115.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace MCXV\SimpleCart\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'sc_orders';    
+    protected $fillable = [
+        'order_number', 'status', 'total_amount'
+    ];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+    
+}
